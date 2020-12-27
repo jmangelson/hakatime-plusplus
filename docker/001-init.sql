e1185819e378410588d089b29e2c71f26719bfde
Joshua Mangelson <mangelson@byu.edu>
2020-12-27 13:47:27 -0700

    Added preliminary addtional tables to db for goals, tasks, major-sub-sub_sub categories

--- docker/001-init.sql
+++ docker/001-init.sql
@@ -48,4 +48,58 @@ BEGIN;
         time_sent TIMESTAMP NOT NULL
     );
 
+    CREATE TABLE IF NOT EXISTS major_categories (
+        id SERIAL PRIMARY KEY,    
+        name TEXT,
+        owner TEXT REFERENCES users(username)
+    );
+
+    CREATE TABLE IF NOT EXISTS sub_categories (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        major_category_id INT REFERENCES major_categories(id)
+    );
+
+    CREATE TABLE IF NOT EXISTS sub_sub_categories (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        major_category_id INT REFERENCES major_categories(id),
+        sub_category_id INT REFERENCES sub_categories(id)                
+    );
+
+    CREATE TABLE IF NOT EXISTS task_states (
+        name TEXT PRIMARY KEY
+    );
+
+    CREATE TABLE IF NOT EXISTS classes (
+        name TEXT PRIMARY KEY
+    );
+
+    CREATE TABLE IF NOT EXISTS tasks (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        major_category_id INT REFERENCES major_categories(id),
+        sub_category_id INT REFERENCES sub_categories(id),
+        sub_sub_category_id INT REFERENCES sub_sub_categories(id),
+        status TEXT REFERENCES task_states(name),
+        class TEXT REFERENCES classes(name),
+        time_created TIMESTAMP NOT NULL,
+        deadline TIMESTAMP NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS goal_classes (
+        id SERIAL PRIMARY KEY,    
+        name TEXT 
+    );
+
+    CREATE TABLE IF NOT EXISTS goals (
+        id SERIAL PRIMARY KEY,
+        name TEXT,
+        goal_type TEXT, 
+        time_created TIMESTAMP NOT NULL,
+        start_time TIMESTAMP NOT NULL,
+        end_time TIMESTAMP NOT NULL
+    );
+
+
 COMMIT;
